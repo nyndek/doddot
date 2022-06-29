7aca4a517df16b0b8a2ef7b7f7c95fd7ae788b6e
Kennedy <kennedy01101@gmail.com>
2022-06-29 00:47:39 -0300

    Refactor code

--- doddot/helpers.py
+++ doddot/helpers.py
@@ -1,46 +1,49 @@
-from commands import TODO, DOING, DONE
 import os
-import platform
+
+from commands import doing, done, todo
+
 
 def clear_screen():
     os.system('clear||cls')
 
-def check_path():
-    dir_path = 'data/'
-    file_name = 'taskdb'
-    full_path = dir_path + file_name
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
-    if not os.path.exists(full_path):
-        with open(os.path.join(dir_path, file_name), 'x') as teste:
+
+def check_db():
+    dirname = 'data/'
+    filename = 'taskdb'
+    fullpath = dirname + filename
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+    if not os.path.exists(fullpath):
+        with open(os.path.join(dirname, filename), 'x') as teste:
             pass
 
-def check_lines():
+
+def read_db():
     try:
         with open('data/taskdb', 'r+') as data:
             lines = data.readlines()
             if len(lines) > 0:
-                todo_tam = len(lines[0].split(','))
-                if todo_tam != 0:
-                    for i in range(todo_tam):
-                        TODO.append(lines[0].split(',')[i])
-                        if '\n' in TODO:
-                            TODO.remove('\n')
+                todo_length = len(lines[0].split(','))
+                if todo_length != 0:
+                    for i in range(todo_length):
+                        todo.append(lines[0].split(',')[i])
+                        if '\n' in todo:
+                            todo.remove('\n')
             if len(lines) > 1:
-                doing_tam = len(lines[1].split(','))
-                if doing_tam != 0:
-                    for i in range(doing_tam):
-                        DOING.append(lines[1].split(',')[i])
-                        if '\n' in DOING:
-                            DOING.remove('\n')
+                doing_length = len(lines[1].split(','))
+                if doing_length != 0:
+                    for i in range(doing_length):
+                        doing.append(lines[1].split(',')[i])
+                        if '\n' in doing:
+                            doing.remove('\n')
             if len(lines) > 2:
-                done_tam = len(lines[2].split(','))
-                if done_tam != 0:
-                    for i in range(done_tam):
-                        DONE.append(lines[2].split(',')[i])
-                        if '\n' in DONE:
-                            DONE.remove('\n')
-                        if '' in DONE:
-                            DONE.remove('')
+                done_length = len(lines[2].split(','))
+                if done_length != 0:
+                    for i in range(done_length):
+                        done.append(lines[2].split(',')[i])
+                        if '\n' in done:
+                            done.remove('\n')
+                        if '' in done:
+                            done.remove('')
     except FileNotFoundError as err:
-        print("Algum problema ao ler o arquivo {}".format(err.filename))
+        print('An error has occurred: {}'.format(err.filename))
