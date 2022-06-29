8b8260b2dfedf992dcdfd5acd1c6da68fb46a6a2
Kennedy <kennedy01101@gmail.com>
2022-06-29 00:50:37 -0300

    Refactor code

--- doddot/commands.py
+++ doddot/commands.py
@@ -1,158 +1,160 @@
-TODO = []
-DOING = []
-DONE = []
+todo = []
+doing = []
+done = []
+
 
 def show_tasks():
     try:
         with open('data/taskdb', 'r') as data:
             lines = data.readlines()
             if len(lines) > 0:
-                todo = lines[0]
-                todo_len = len(lines[0].split(',')) - 1
+                todo_length = len(lines[0].split(',')) - 1
                 print('TODO:')
-                for item in range(todo_len):
-                    print("\t[ ] {}".format(lines[0].split(',')[item]))
+                for item in range(todo_length):
+                    print('\t[ ] {}'.format(lines[0].split(',')[item]))
                 print('')
             else:
                 print('TODO:')
             if len(lines) > 1:
-                doing = lines[1]
-                doing_len = len(lines[1].split(',')) - 1
+                doing_length = len(lines[1].split(',')) - 1
                 print('DOING:')
-                for item in range(doing_len):
-                    print("\t[..] {}".format(lines[1].split(',')[item]))
+                for item in range(doing_length):
+                    print('\t[..] {}'.format(lines[1].split(',')[item]))
                 print('')
             else:
                 print('DOING:')
             if len(lines) > 2:
-                done = lines[2]
-                done_len = len(lines[2].split(',')) - 1
+                done_length = len(lines[2].split(',')) - 1
                 print('DONE:')
-                for item in range(done_len):
-                    print("\t[x] {}".format(lines[2].split(',')[item]))
+                for item in range(done_length):
+                    print('\t[x] {}'.format(lines[2].split(',')[item]))
                 print('')
             else:
                 print('DONE:')
-    except FileNotFoundError:
-        pass
+    except FileNotFoundError as err:
+        print('An error has occurred: {}'.format(err.filename))
 
-def add_task(task):
-    if task == '':
+
+def add_task(text):
+    if text == '':
         return
-    command = task.split(' ')[-1]
+    command = text.split(' ')[-1]
     if command == 'todo':
-        task_name = task.split(' ')
-        task_name.remove('todo')
-        s_task = ''
-        for i in range(len(task_name)):
-            s_task += task_name[i] + ' '
-        TODO.append(s_task)
+        words = text.split(' ')
+        words.remove('todo')
+        task = ''
+        for i in range(len(words)):
+            task += words[i] + ' '
+        todo.append(task)
     if command == 'doing':
-        task_name = task.split(' ')
-        task_name.remove('doing')
-        s_task = ''
-        for i in range(len(task_name)):
-            s_task += task_name[i] + ' '
-        DOING.append(s_task)
+        words = text.split(' ')
+        words.remove('doing')
+        task = ''
+        for i in range(len(words)):
+            task += words[i] + ' '
+        doing.append(task)
     if command == 'done':
-        task_name = task.split(' ')
-        task_name.remove('done')
-        s_task = ''
-        for i in range(len(task_name)):
-            s_task += task_name[i] + ' '
-        DONE.append(s_task)
+        words = text.split(' ')
+        words.remove('done')
+        task = ''
+        for i in range(len(words)):
+            task += words[i] + ' '
+        done.append(task)
     try:
         with open('data/taskdb', 'r+') as data:
-            for i in range(len(TODO)):
-                data.write("{},".format(TODO[i]))
+            for i in range(len(todo)):
+                data.write('{},'.format(todo[i]))
             data.write('\n')
-            for i in range(len(DOING)):
-                data.write("{},".format(DOING[i]))
+            for i in range(len(doing)):
+                data.write('{},'.format(doing[i]))
             data.write('\n')
-            for i in range(len(DONE)):
-                data.write("{},".format(DONE[i]))
-    except FileNotFoundError:
-        pass
+            for i in range(len(done)):
+                data.write('{},'.format(done[i]))
+    except FileNotFoundError as err:
+        print('An error has occurred: {}'.format(err.filename))
 
-def move_task(task):
-    if task == '':
+
+def move_task(text):
+    if text == '':
         return
-    command = task.split(' ')[-1]
+    command = text.split(' ')[-1]
     if command == 'todo':
-        task_ = task.split(' ')
-        task_.remove('todo')
-        s_task = ''
-        for i in range(len(task_)):
-            s_task += task_[i] + ' '
-        if s_task in DOING:
-            DOING.remove(s_task)
-        elif s_task in DONE:
-            DONE.remove(s_task)
-        TODO.append(s_task)
+        words = text.split(' ')
+        words.remove('todo')
+        task = ''
+        for i in range(len(words)):
+            task += words[i] + ' '
+        if task in doing:
+            doing.remove(task)
+        elif task in done:
+            done.remove(task)
+        todo.append(task)
     if command == 'doing':
-        task_ = task.split(' ')
-        task_.remove('doing')
-        s_task = ''
-        for i in range(len(task_)):
-            s_task += task_[i] + ' '
-        if s_task in TODO:
-            TODO.remove(s_task)
-        elif s_task in DONE:
-            DONE.remove(s_task)
-        DOING.append(s_task)
+        words = text.split(' ')
+        words.remove('doing')
+        task = ''
+        for i in range(len(words)):
+            task += words[i] + ' '
+        if task in todo:
+            todo.remove(task)
+        elif task in done:
+            done.remove(task)
+        doing.append(task)
     if command == 'done':
-        task_ = task.split(' ')
-        task_.remove('done')
-        s_task = ''
-        for i in range(len(task_)):
-            s_task += task_[i] + ' '
-        if s_task in TODO:
-            TODO.remove(s_task)
-        elif s_task in DOING:
-            DOING.remove(s_task)
-        DONE.append(s_task)
+        words = text.split(' ')
+        words.remove('done')
+        task = ''
+        for i in range(len(words)):
+            task += words[i] + ' '
+        if task in todo:
+            todo.remove(task)
+        elif task in doing:
+            doing.remove(task)
+        done.append(task)
     try:
         with open('data/taskdb', 'w') as data:
-            for i in range(len(TODO)):
-                data.write("{},".format(TODO[i]))
+            for i in range(len(todo)):
+                data.write('{},'.format(todo[i]))
             data.write('\n')
-            for i in range(len(DOING)):
-                data.write("{},".format(DOING[i]))
+            for i in range(len(doing)):
+                data.write('{},'.format(doing[i]))
             data.write('\n')
-            for i in range(len(DONE)):
-                data.write("{},".format(DONE[i]))
-    except FileNotFoundError:
-        pass
+            for i in range(len(done)):
+                data.write('{},'.format(done[i]))
+    except FileNotFoundError as err:
+        print('An error has occurred: {}'.format(err.filename))
 
-def delete_task(task_):
-    if task_ == '':
+
+def delete_task(text):
+    if text == '':
         return
-    task = task_ + ' '
-    if task in TODO:
-        TODO.remove(task)
-    if task in DOING:
-        DOING.remove(task)
-    if task in DONE:
-        DONE.remove(task)
+    task = text + ' '
+    if task in todo:
+        todo.remove(task)
+    if task in doing:
+        doing.remove(task)
+    if task in done:
+        done.remove(task)
     try:
         with open('data/taskdb', 'w') as data:
-            for i in range(len(TODO)):
-                data.write("{},".format(TODO[i]))
+            for i in range(len(todo)):
+                data.write('{},'.format(todo[i]))
             data.write('\n')
-            for i in range(len(DOING)):
-                data.write("{},".format(DOING[i]))
+            for i in range(len(doing)):
+                data.write('{},'.format(doing[i]))
             data.write('\n')
-            for i in range(len(DONE)):
-                data.write("{},".format(DONE[i]))
-    except FileNotFoundError:
-        pass
+            for i in range(len(done)):
+                data.write('{},'.format(done[i]))
+    except FileNotFoundError as err:
+        print('An error has occurred: {}'.format(err.filename))
+
 
 def delete_all():
-    TODO.clear()
-    DOING.clear()
-    DONE.clear()
+    todo.clear()
+    doing.clear()
+    done.clear()
     try:
         with open('data/taskdb', 'w') as data:
             data.write('')
-    except FileNotFoundError:
-        pass
+    except FileNotFoundError as err:
+        print('An error has occurred: {}'.format(err.filename))
